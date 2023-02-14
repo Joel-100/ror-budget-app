c652c5e78639d7b03df54c2846a284baf984ad88
Joel-100 <joelkugonza@gmail.com>
2023-02-14 17:08:23 +0300

    add form to add a category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,5 +5,21 @@ class GroupsController < ApplicationController
     @groups = current_user.groups
   end
 
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.author = current_user
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to user_groups_path(current_user), notice: 'Group was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
 
 end
