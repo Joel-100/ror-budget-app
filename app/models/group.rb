a8169c92fa96537491a0c1fb4c383c031d218254
Joel-100 <joelkugonza@gmail.com>
2023-02-14 12:09:54 +0300

    validate group attributes

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :group_entities, dependent: :destroy
   has_many :entities, through: :group_entities
+  
+  validates :name, presence: true
+  validates :icon, presence: true
 end
