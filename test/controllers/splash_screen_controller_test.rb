2c62a8cdb633b5185192b29dd83964df8b77df2f
Joel-100 <joelkugonza@gmail.com>
2023-02-14 16:14:11 +0300

    fix-linters

--- test/controllers/splash_screen_controller_test.rb
+++ test/controllers/splash_screen_controller_test.rb
@@ -1,7 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class SplashScreenControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get splash_screen_index_url
     assert_response :success
   end
