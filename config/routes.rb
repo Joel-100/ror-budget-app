f015a01b9f5f514093addc918239b3040d322295
Joel-100 <joelkugonza@gmail.com>
2023-02-14 13:18:27 +0300

    created splash-screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  root 'splash_screen#index'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
