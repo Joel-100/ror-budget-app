efcc1b9df9bd83937814709e8f1a17aba7821b28
Joel-100 <joelkugonza@gmail.com>
2023-02-14 12:05:40 +0300

    create relations for entity model

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,5 @@
 class Entity < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: "User"
+  has_many :group_entities, dependent: :destroy
+  has_many :groups, through: :group_entities
 end
