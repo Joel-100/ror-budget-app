c458cb84f3ea4be294c26453e97218d3505bade2
Joel-100 <joelkugonza@gmail.com>
2023-02-14 12:04:00 +0300

    add relations to the model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: "User"
+  has_many :group_entities, dependent: :destroy
+  has_many :entities, through: :group_entities
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :groups, foreign_key: "author_id"
-  has_many :entities, foreign_key: "author_id"
+  has_many :groups, foreign_key: "author_id", dependent: :destroy
+  has_many :entities, foreign_key: "author_id", dependent: :destroy
 end
