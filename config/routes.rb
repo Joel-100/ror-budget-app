d6508d330be55dffa007b56b5212288344092a14
Joel-100 <joelkugonza@gmail.com>
2023-02-14 14:08:51 +0300

    created login and sign up pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root 'splash_screen#index'
   devise_for :users
+
+  resources :users do
+    resources :groups do
+      resources :entities
+      resources :group_entities
+    end
+  end
+
 end
