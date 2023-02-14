f9cd304cf920b1a4e4ac8ef23ec9ba0acef21d52
Joel-100 <joelkugonza@gmail.com>
2023-02-14 12:11:08 +0300

    validate name and amount

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -2,4 +2,7 @@ class Entity < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :group_entities, dependent: :destroy
   has_many :groups, through: :group_entities
+
+  validates :name, presence: true
+  validates :amount, presence: true
 end
